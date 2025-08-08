dbbee6ddba46b7d55141a21baaaa3044aad1d272
Ibramsou <kasobanboui@gmail.com>
2025-08-08 09:42:06 +0000

    Add 1.21.8 Support

--- Plugin/src/main/java/net/seanomik/tamablefoxes/TamableFoxes.java
+++ Plugin/src/main/java/net/seanomik/tamablefoxes/TamableFoxes.java
@@ -32,6 +32,7 @@ import java.io.*;
 import java.util.logging.Level;
 import net.seanomik.tamablefoxes.versions.version_1_21_4_R1.NMSInterface_1_21_4_R1;
 import net.seanomik.tamablefoxes.versions.version_1_21_5_R1.NMSInterface_1_21_5_R1;
+import net.seanomik.tamablefoxes.versions.version_1_21_8_R1.NMSInterface_1_21_8_R1;
 
 public final class TamableFoxes extends JavaPlugin implements Listener {
     private static TamableFoxes plugin;
@@ -77,6 +78,7 @@ public final class TamableFoxes extends JavaPlugin implements Listener {
             case "1.21", "1.21.1" -> nmsInterface = new NMSInterface_1_21_R1(); // FOX
             case "1.21.4" -> nmsInterface = new NMSInterface_1_21_4_R1();
             case "1.21.5" -> nmsInterface = new NMSInterface_1_21_5_R1();
+            case "1.21.8" -> nmsInterface = new NMSInterface_1_21_8_R1();
 
             default -> {
                 Bukkit.getServer().getConsoleSender().sendMessage(Config.getPrefix() + ChatColor.RED + LanguageConfig.getUnsupportedMCVersionRegister());
